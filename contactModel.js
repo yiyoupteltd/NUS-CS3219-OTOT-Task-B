c72a35abf31fc48fc72483c84e6baa090bff6fd6
yiyoupteltd <48853932+yiyoupteltd@users.noreply.github.com>
2020-10-07 23:20:57 +0800

    Add files via upload

---
+++ contactModel.js
@@ -0,0 +1,34 @@
+// contactModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: String,
+    phone: String,
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+contactSchema.pre('save', next => {
+    now = new Date();
+    if(!this.createdAt) {
+      this.createdAt = now;
+    }
+    next();
+  });
+
+  
+// Export Contact model
+var Contact = module.exports = mongoose.model('contact', contactSchema);
+module.exports.get = function (callback, limit) {
+    Contact.find(callback).limit(limit);
+}
